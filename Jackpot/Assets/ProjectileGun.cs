e4685c020e8bccc96dcc61fc7490e5d963ecb847
Nalia <76820008+Support-Crispy@users.noreply.github.com>
2024-11-06 08:49:15 -0500

    kablammy

---
+++ Jackpot/Assets/ProjectileGun.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileGun : MonoBehaviour
+{
+    //bullet
+    public GameObject bullet;
+
+    //bullet force
+    public float shootForce, upwardForce;
+
+    //Gun Stats
+    public float timeBetweenShooting, spread, reloadTime, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+
+    int bulletsLeft, bulletsShot;
+
+    //bools
+    bool shooting, readyToShoot, reloading;
+
+    //Reference
+    public Camera fpsCam;
+    public Transform attackPoint;
+
+    //Bug Fixing :p
+    public bool allowInvoke = true;
+
+    private void Awake()
+    {
+        //make sure magazine is full
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+
+    private void Update()
+    {
+        MyInput();
+    }
+
+    private void MyInput()
+    {
+        //Check if autofire is on and get the input
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        //Reloading
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading) Reload();
+
+        //reload automatically when trying to shoot without ammo
+        if (readyToShoot && shooting && !reloading && bulletsLeft <= 0) Reload();
+
+        //Shooting (Waow!)
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            //Set bullets fired to 0
+            bulletsShot = 0;
+
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        //Find the exact hit position with a raycsat
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); //This is just a ray through the middle of your camera
+        RaycastHit hit;
+
+        //Check if the ray hits something
+        Vector3 targetPoint;
+        if(Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else
+            targetPoint = ray.GetPoint(75); //just some arbitrary point far away
+
+        //calculate direction from attack point to target point
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+
+        //calculate spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        //calculate new direction w/spread
+        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0); //Just add the last direction lmao
+
+        //gameobject instantiation
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+
+        //rotate bullet
+        currentBullet.transform.forward = directionWithSpread.normalized;
+
+        //add forces to projectile
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
+        currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
+
+        //ammo stuff
+        bulletsLeft--;
+        bulletsShot++;
+
+        //Invoke reset shot function
+        if (allowInvoke)
+        {
+            Invoke("ResetShot", timeBetweenShooting);
+            allowInvoke = false;
+        }
+
+        //if more than one bullets per tap make sure to repeat shoot function
+        if (bulletsShot < bulletsPerTap && bulletsLeft > 0)
+            Invoke("Shoot", timeBetweenShots);
+    }
+
+    private void ResetShot()
+    {
+        //allow shooting and invoking again
+        readyToShoot = true;
+        allowInvoke = true;
+    }
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
+
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+}
